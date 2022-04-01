e6b41ea52fa3ae4a9817818c1660734652209eff
dhruv50ae <linkdp2012@gmail.com>
2022-04-01 18:07:03 +0100

    Implemented Geolocations

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,14 @@
 import React from "react";
 
 const SeasonDisplay = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position);
+    },
+    (err) => {
+      console.log(err);
+    }
+  );
   return <div>SeasonDisplay</div>;
 };
 
