56307637ea39ad794bbb1bfe9bfeb4e6e1ad724c
dhruv50ae <linkdp2012@gmail.com>
2022-04-02 15:31:08 +0100

    Added conditional rendering

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 
 export default class SeasonDisplay extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
     window.navigator.geolocation.getCurrentPosition(
       (position) => {
-        console.log(position);
+        this.setState({ lat: position.coords.latitude });
       },
       (err) => {
-        console.log(err);
+        this.setState({ errorMessage: err.message });
       }
     );
-    return <div>Latitude: </div>;
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>Loading!</div>;
   }
 }
