06b9a62a872707fe04c5e367f4c82cdf8461233e
dhruv50ae <linkdp2012@gmail.com>
2022-04-02 17:29:54 +0100

    Added conditionally rendered styles

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./SeasonDisplay.css";
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -23,10 +24,10 @@ const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   const { text, iconName } = seasonConfig[season];
   return (
-    <div>
-      <i className={`${iconName} icon`}></i>
+    <div className={`season-display ${season}`}>
+      <i className={`${iconName} icon-left massive icon`}></i>
       <h1>{text}</h1>
-      <i className={`${iconName} icon`}></i>
+      <i className={`${iconName} icon-right massive icon`}></i>
     </div>
   );
 };
