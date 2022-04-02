e6333a30b989ead9c2de93f0bf48d12fc3bffed8
dhruv50ae <linkdp2012@gmail.com>
2022-04-02 17:38:21 +0100

    Created a default fallback for props.message

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ export default class App extends Component {
     }
     return (
       <div>
-        <Spinner />
+        <Spinner message="Please accept the location request" />
       </div>
     );
   }
