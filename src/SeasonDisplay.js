31dd6d5aec5d3763d8519a4d7357eeab541440e5
dhruv50ae <linkdp2012@gmail.com>
2022-04-02 15:55:42 +0100

    Changes to App and SeasonDisplay
    Needed to change App and SeasonDisplay so that they have appropriate based components

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,26 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class SeasonDisplay extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errorMessage: "" };
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errorMessage: err.message });
-      }
-    );
-  }
+const SeasonDisplay = (props) => {
+  return <div>SeasonDisplay</div>;
+};
 
-  render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <div>Latitude: {this.state.lat}</div>;
-    }
-    return <div>Loading!</div>;
-  }
-}
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  return (
-    <div>
-      <SeasonDisplay />
-    </div>
-  );
-};
+export default class App extends Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return (
+        <div>
+          <SeasonDisplay lat={this.state.lat} />
+        </div>
+      );
+    }
+    return <div>Loading!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
